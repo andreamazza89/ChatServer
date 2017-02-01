b5d6728e3b366995193c0ef47bdfe1405fc338b2
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2017-02-01 15:58:08 +0000

    Add test for echo server

--- src/main/java/EchoServer.java
+++ src/main/java/EchoServer.java
@@ -4,23 +4,31 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class EchoServer {
-    public static void main(String[] args) {
-        try (
-            ServerSocket serverSocket = new ServerSocket(2222);
-            Socket clientSocket = serverSocket.accept();
-            BufferedReader fromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            PrintStream toClient = new PrintStream(clientSocket.getOutputStream());
-        ) {
-            String message_received;
+class EchoServer {
 
-            while ((message_received = fromClient.readLine()) != null) {
-                toClient.println(message_received);
-                System.out.println("~> " + message_received);
+    void run(int portNumber) {
+        ExecutorService connections = Executors.newCachedThreadPool();
+
+        connections.submit(() -> {
+            try (
+                ServerSocket serverSocket = new ServerSocket(portNumber);
+                Socket clientSocket = serverSocket.accept();
+                BufferedReader fromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintStream toClient = new PrintStream(clientSocket.getOutputStream())
+            ) {
+                String message_received;
+
+                while ((message_received = fromClient.readLine()) != null) {
+                    toClient.println(message_received);
+                    System.out.println("~> " + message_received);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        });
+
     }
 }
