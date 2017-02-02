abc7caf8a5c064673541488202304dc6f031303d
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2017-02-02 11:05:23 +0000

    Clean files for a fresh test-drive start

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,2 @@
 public class Main {
-    public static void main(String[] args) {
-        EchoServer server = new EchoServer();
-        server.run(2222);
-    }
 }
--- src/test/java/EchoServerShould.java
+++ src/test/java/EchoServerShould.java
@@ -1,28 +1,2 @@
-import org.junit.Test;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.net.Socket;
-
-import static org.junit.Assert.assertEquals;
-
 public class EchoServerShould {
-
-    @Test
-    public void pipeAMessageBackToTheSender() throws IOException {
-        EchoServer server = new EchoServer();
-        server.run(1111);
-
-        Socket testClientSocket = new Socket("localhost", 1111);
-        PrintStream toServer = new PrintStream(testClientSocket.getOutputStream());
-        BufferedReader fromServer = new BufferedReader(new InputStreamReader(testClientSocket.getInputStream()));
-
-        toServer.println("Geronimo!");
-        String response = fromServer.readLine();
-
-        assertEquals("Geronimo!", response);
-        testClientSocket.close();
-    }
 }
